ed9e53e9b691e32cbbfc50d23ef7796cf81c0945
RafaelMisuraca <rafaelmisuraca@gmail.com>
2024-11-18 16:46:55 -0300

    Add files via upload

---
+++ avaliacao.py
@@ -0,0 +1,88 @@
+porta1 = 2
+porta2 = 4
+porta3 = 6
+porta4 = 8
+porta5 = 10
+porta6 = 12
+porta7 = 14
+
+palpite1 = int(input("Digite aqui o seu número: "))
+while palpite1:
+  if palpite1 == porta1:
+    print("Você liberou a porta 1")
+    break
+  elif palpite1 != porta1:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
+palpite2 = int(input("Digite aqui o seu número: "))
+while palpite2:
+  if palpite2 == porta2:
+    print("Você liberou a porta 2")
+    break
+  elif palpite2 != porta2:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
+palpite3 = int(input("Digite aqui o seu número: "))
+while palpite3:
+  if palpite3 == porta3:
+    print("Você liberou a porta 3")
+    break
+  elif palpite3 != porta3:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
+palpite4 = int(input("Digite aqui o seu número: "))
+while palpite4:
+  if palpite4 == porta4:
+    print("Você liberou a porta 4")
+    break
+  elif palpite4 != porta4:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
+
+palpite5 = int(input("Digite aqui o seu número: "))
+while palpite5:
+  if palpite5 == porta5:
+    print("Você liberou a porta 5")
+    break
+  elif palpite5 != porta5:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
+
+palpite6 = int(input("Digite aqui o seu número: "))
+while palpite6:
+  if palpite6 == porta6:
+    print("Você liberou a porta 6")
+    break
+  elif palpite6 != porta6:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
+
+palpite7 = int(input("Digite aqui o seu número: "))
+while palpite7:
+  if palpite7 == porta7:
+    print("Você liberou a porta 7")
+    break
+  elif palpite7 != porta7:
+    print("Você errou, tente denovo. Encerre o programa para reiniciar os números")
+    break
+  else:
+    print("Número inválido.")
+    break
---
+++ avaliacao2.py
@@ -0,0 +1,8 @@
+soma = 0 
+
+contador = 0 
+
+while contador < 50:
+  contador +=1
+  soma += contador*2
+print(soma)
